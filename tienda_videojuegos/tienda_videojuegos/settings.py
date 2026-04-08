efe93f9d214bb5228ab3293ba0acbca5b3097808
MauroDev <thorsteinmv@gmail.com>
2026-04-08 14:37:08 -0500

    crear una plantilla base en Django y reutilizar contenido

--- tienda_videojuegos/tienda_videojuegos/settings.py
+++ tienda_videojuegos/tienda_videojuegos/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tienda_videojuegos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
