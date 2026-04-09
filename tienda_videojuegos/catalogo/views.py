8a44ad7553a793b65e75fe793b8a29c885790f4e
MauroDev <thorsteinmv@gmail.com>
2026-04-09 18:03:21 -0500

    paginacion en django para catalogos de productos

--- tienda_videojuegos/catalogo/views.py
+++ tienda_videojuegos/catalogo/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Juego
 
+
 def lista_juegos(request):
-   juegos = Juego.objects.all()
-   contexto_catalogo_juegos = {'lista_juegos': juegos}
-   return render(request, 'catalogo/lista_juegos.html', contexto_catalogo_juegos)
+    # 1. Query base (ordenado)
+    juegos = Juego.objects.all().order_by('id')
+
+    # 2. Paginador
+    paginator = Paginator(juegos, 6)
+
+    # 3. Página actual
+    page_number = request.GET.get('page')
+    lista_juegos = paginator.get_page(page_number)
+
+    # 4. Lógica de rango (para evitar lógica en el template)
+    current_page = lista_juegos.number
+    total_pages = paginator.num_pages
+
+    rango_paginas = [
+        num for num in paginator.page_range
+        if current_page - 2 <= num <= current_page + 2
+    ]
+
+    # 5. Contexto
+    contexto = {
+        'lista_juegos': lista_juegos,
+        'rango_paginas': rango_paginas,
+        'total_paginas': total_pages
+    }
+
+    return render(request, 'catalogo/lista_juegos.html', contexto)
