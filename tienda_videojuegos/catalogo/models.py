8fe48cae3b7267b704615397ee415d04fc90c4bb
MauroDev <thorsteinmv@gmail.com>
2026-04-09 15:08:25 -0500

    se añade atributo imagen al modelo Juegos y se colocan imagenes en la carpeta static-img-portadas

--- tienda_videojuegos/catalogo/models.py
+++ tienda_videojuegos/catalogo/models.py
@@ -4,6 +4,7 @@ class Juego(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=0)
     plataforma = models.CharField(max_length=200)
+    imagen = models.CharField(max_length=200, default="default.png")
     
     def __str__(self):
         return self.nombre
