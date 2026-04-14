6176e6fb27e6db4c3f5c3edf97631cc589444d2f
MauroDev <thorsteinmv@gmail.com>
2026-04-14 15:01:32 -0500

    badge carrito funcional

--- tienda_videojuegos/carrito/context_processors.py
+++ tienda_videojuegos/carrito/context_processors.py
@@ -1,10 +1,17 @@
-from .models import Carrito
+from .models import Carrito, ItemCarrito
+
 
 def carrito_total(request):
     total_items = 0
-    if request.user.is_authenticated:
-        carrito = Carrito.objects.filter(usuario=request.user).first()
-        if carrito:
-            total_items = carrito.total_items()
-    return {'carrito_total_items': total_items}
-    
+    try:
+        if request.user.is_authenticated:
+            carrito = (
+                Carrito.objects.prefetch_related("items")
+                .filter(usuario=request.user)
+                .first()
+            )
+            if carrito:
+                total_items = ItemCarrito.objects.filter(carrito=carrito).count()
+    except Exception:
+        pass
+    return {"carrito_total_items": total_items}
